1584eefe75c7ef1ab6df3f76eea2a42f7327667b
anthony1234567 <amart088@ucr.edu>
2015-05-11 23:48:01 -0700

    cleaned up code a bit

--- src/letter.h
+++ src/letter.h
@@ -6,28 +6,28 @@
 
 class letter {
 private:
-  // data per letter
-  char character; // representation of letter
-  int frequency; // how many times character is seen in text
-  double probability; // times seen / total chars in text
-  std::string encoding; // alternate codeword for this letter
+	// data per letter
+	char character; // representation of letter
+	int frequency; // how many times character is seen in text
+	double probability; // times seen / total chars in text
+	std::string encoding; // alternate codeword for this letter
 	std::vector<letter*> otherCharsLarger; // for combining letters when encoding
 	std::vector<letter*> otherCharsSmaller; // for combining letters when encoding
 public:
-  letter(char); // constructor ~ seen for first time
-  letter(double); // data is known (for "fusing" 2 letters)
-  letter(char, std::string); // for decoding
-  const char getChar() const; // grab character that represents letter in alphabet
-  const int getFreq() const; // grab frequency of letter
-  const double getProb() const; // grab probability of letter happening
-  const std::string getCode() const; // grab codeword corresponding to letter
-  void incrementFreq(); // everytime a letter is seen
+	letter(char); // constructor ~ seen for first time
+	letter(double); // data is known (for "fusing" 2 letters)
+	letter(char, std::string); // for decoding
+	const char getChar() const; // grab character that represents letter in alphabet
+	const int getFreq() const; // grab frequency of letter
+	const double getProb() const; // grab probability of letter happening
+	const std::string getCode() const; // grab codeword corresponding to letter
+	void incrementFreq(); // everytime a letter is seen
 	void decrimentFreq(); // to remove '\n' node
-  void setProbability(const int &); // update probability
+	void setProbability(const int &); // update probability
 	void encode(char); // does the encoding
 	void code(); // encodes 1 to front of larger subgroup and 0 to smaller
-  letter fuse(letter&); // binary fusion
-  bool operator<(const letter&) const; // sorting purposes
+	letter fuse(letter&); // binary fusion
+	bool operator<(const letter&) const; // sorting purposes
 	void bootlegencode(std::string); // enough said
 };
 
