7f8b6126f0f7cb4587f8e999b3ce0120aabc58be
anthony1234567 <amart088@ucr.edu>
2015-05-26 14:12:33 -0700

    fixed function for progress bar

--- src/functions.cpp
+++ src/functions.cpp
@@ -2,13 +2,19 @@
 #include <iomanip>
 
 static inline void loadbar(unsigned int x, unsigned int n, unsigned int w = 100) {
-	if ( (x != n) && (x % (n/100+1) != 0) ) return;
-	float ratio = x/(float)n;
-	int c = ratio * w;
-	std::cout << std::setw(3) << (int)(ratio*100) << "% [";
-	for (int x=0; x<c; x++) std::cout << "=";
-	for (int x=c; x<w; x++) std::cout << " ";
-	std::cout << "]\r" << std::flush;
+  if ( (x != n) && (x % (n/100+1) != 0) ) {
+    return;
+  }
+  float ratio = x/(float)n;
+  int c = ratio * w;
+  std::cout << std::setw(3) << (int)(ratio*100) << "% [";
+  for (int x = 0; x < c; x++) {
+    std::cout << "=";
+  }
+  for (int x = c; x < w; x++) {
+    std::cout << " ";
+  }
+  std::cout << "]\r" << std::flush;
 }
 
 std::string getFilename(const std::string &path) {
